ac5791eecf5f43f67f06994ffe1d154014e23fce
Abdullah <>
2021-11-11 11:20:49 +0500

    first

---
+++ app/src/main/java/com/example/mc_quiz/quiz_attempt.java
@@ -0,0 +1,14 @@
+package com.example.mc_quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quiz_attempt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_attempt);
+    }
+}
